36349908759a0bb273394f4510cfbdf5987017f4
Fabian Bergström <fabian@fmbb.se>
2019-07-20 13:16:34 +0200

    read normals for models

--- src/model.rs
+++ src/model.rs
@@ -6,11 +6,13 @@ use crate::geometry::Vec3i;
 pub struct Face {
     pub verts: Vec3i,
     pub uvs: Vec3i,
+    pub norms: Vec3i,
 }
 
 pub struct Model {
     verts: Vec<Vec3f>,
     uvs: Vec<Vec3f>,
+    norms: Vec<Vec3f>,
     faces: Vec<Face>,
 }
 
@@ -18,6 +20,7 @@ impl Model {
     pub fn from_obj<R: BufRead>(obj_data: R) -> Model {
         let mut obj_verts = Vec::new();
         let mut obj_uvs = Vec::new();
+        let mut obj_norms = Vec::new();
         let mut obj_faces = Vec::new();
         for line in obj_data.lines() {
             let line = line.unwrap();
@@ -40,19 +43,41 @@ impl Model {
                     );
                     obj_uvs.push(Vec3f {x, y, z})
                 }
+                Some("vn") => {
+                    tokens.next(); // skip an extra space
+                    let (x, y, z) = (
+                        tokens.next().unwrap().parse().unwrap(),
+                        tokens.next().unwrap().parse().unwrap(),
+                        tokens.next().unwrap().parse().unwrap(),
+                    );
+                    obj_norms.push(Vec3f {x, y, z})
+                }
                 Some("f") => {
                     let (mut a, mut b, mut c) = (
                         tokens.next().unwrap().split('/'),
                         tokens.next().unwrap().split('/'),
                         tokens.next().unwrap().split('/'),
                     );
-                    let (v0, uv0): (i32, i32) = ( a.next().unwrap().parse().unwrap(), a.next().unwrap().parse().unwrap());
-                    let (v1, uv1): (i32, i32) = ( b.next().unwrap().parse().unwrap(), b.next().unwrap().parse().unwrap());
-                    let (v2, uv2): (i32, i32) = ( c.next().unwrap().parse().unwrap(), c.next().unwrap().parse().unwrap());
+                    let (v0, uv0, n0): (i32, i32, i32) = (
+                        a.next().unwrap().parse().unwrap(),
+                        a.next().unwrap().parse().unwrap(),
+                        a.next().unwrap().parse().unwrap(),
+                    );
+                    let (v1, uv1, n1): (i32, i32, i32) = (
+                        b.next().unwrap().parse().unwrap(),
+                        b.next().unwrap().parse().unwrap(),
+                        b.next().unwrap().parse().unwrap(),
+                    );
+                    let (v2, uv2, n2): (i32, i32, i32) = (
+                        c.next().unwrap().parse().unwrap(),
+                        c.next().unwrap().parse().unwrap(),
+                        c.next().unwrap().parse().unwrap(),
+                    );
                     // in wavefront obj all indices start at 1, not zero
                     let face = Face {
                         verts: Vec3i { x: v0-1, y: v1-1, z: v2-1 },
                         uvs: Vec3i { x: uv0-1, y: uv1-1, z: uv2-1 },
+                        norms: Vec3i { x: n0-1, y: n1-1, z: n2-1},
                     };
                     obj_faces.push(face);
                 }
@@ -62,14 +87,9 @@ impl Model {
         Model {
             verts: obj_verts,
             uvs: obj_uvs,
+            norms: obj_norms,
             faces: obj_faces,
         }
-    }
-    pub fn nverts(&self) -> usize {
-        self.verts.len()
-    }
-    pub fn nfaces(&self) -> usize {
-        self.faces.len()
     }
     pub fn vert(&self, i: usize) -> Vec3f {
         self.verts[i]
@@ -82,5 +102,8 @@ impl Model {
     }
     pub fn uv(&self, i: usize) -> Vec3f {
         self.uvs[i]
+    }
+    pub fn norm(&self, i: usize) -> Vec3f {
+        self.norms[i]
     }
 }
